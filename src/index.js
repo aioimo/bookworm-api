b13d889020c3183e03d300a0e1c3c59500b08100
Anthony Ioimo <anthonylioimo@gmail.com>
2019-02-21 11:53:02 +0100

    implement login and logout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+import auth from "./routes/auth";
+
+dotenv.config();
 
 const app = express();
 
-app.post("/api/auth", (req, res) => {
-  console.log("/api/auth pinged");
-  res.status(400).json({ errors: { global: "Invalid credentials" } });
-});
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+
+app.use("/api/auth", auth);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
